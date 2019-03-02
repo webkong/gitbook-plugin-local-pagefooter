d34fdae9e6ef00f18d7b7acf9b646ae6e25f4b8a
webkong <772930508@qq.com>
2019-03-02 15:35:40 +0800

    Fix copyright

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ module.exports = {
     ],
   },
   hooks: {
-    'page:before': function(page) {
+    'page:before': function (page) {
       var _label = 'File Modify: ',
-          _format = 'YYYY-MM-DD HH:mm:ss',
-          _copy = 'powered by Gitbook',
-          _islocal = true;
-      if(this.options.pluginsConfig['local-pagefooter']) {
+        _format = 'YYYY-MM-DD HH:mm:ss',
+        _copy = 'powered by Gitbook',
+        _islocal = true;
+      if (this.options.pluginsConfig['local-pagefooter']) {
         _label = this.options.pluginsConfig['local-pagefooter']['modify_label'] || _label;
         _format = this.options.pluginsConfig['local-pagefooter']['modify_format'] || _format;
 
         var _c = this.options.pluginsConfig['local-pagefooter']['copyright'];
-        _copy = _c ? _c + ' all right reserved，' + _copy : _copy;
+        _copy = _c ? '@' + _c + ' ' + _copy : _copy;
       }
-      var _copy = '<span class="copyright">'+_copy+'</span>'
+      var _copy = '<span class="copyright">' + _copy + '</span>'
       var str = ' \n\n<footer class="page-footer">' + _copy +
         '<span class="footer-modification">' +
         _label +
@@ -30,12 +30,12 @@ module.exports = {
     }
   },
   filters: {
-    date: function(d, format) {
+    date: function (d, format) {
       var _islocal = this.options.pluginsConfig['local-pagefooter']['islocal'] || _islocal;
       var time = '';
-      if (_islocal){
+      if (_islocal) {
         time = moment(d).local().format(format)
-      }else{
+      } else {
         time = moment(d).format(format)
       }
       return time
